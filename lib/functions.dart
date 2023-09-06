c1ae08327f45674196b676b8916fcc775f55598c
Jazeb_Javed <jazebjaved52@gmail.com>
2023-09-06 17:32:11 +0500

    Full cart screen done

--- lib/functions.dart
+++ lib/functions.dart
@@ -1,24 +1,24 @@
+// Convert String into Sentence Case Function
 String toSentenceCase(String text) {
-    // ignore: unnecessary_null_comparison
-    if (text == null || text.isEmpty) {
+  // ignore: unnecessary_null_comparison
+  if (text == null || text.isEmpty) {
+    return "";
+  }
+
+  // Split the text into words
+  List<String> words = text.split(' ');
+
+  // Capitalize the first letter of each word
+  List<String> sentenceCaseWords = words.map((word) {
+    if (word.isNotEmpty) {
+      return word[0].toUpperCase() + word.substring(1).toLowerCase();
+    } else {
       return "";
     }
+  }).toList();
 
-    // Split the text into words
-    List<String> words = text.split(' ');
+  // Join the words back together with spaces
+  String sentenceCaseText = sentenceCaseWords.join(' ');
 
-    // Capitalize the first letter of each word
-    List<String> sentenceCaseWords = words.map((word) {
-      if (word.isNotEmpty) {
-        return word[0].toUpperCase() + word.substring(1).toLowerCase();
-      } else {
-        return "";
-      }
-    }).toList();
-
-    // Join the words back together with spaces
-    String sentenceCaseText = sentenceCaseWords.join(' ');
-
-    return sentenceCaseText;
+  return sentenceCaseText;
 }
-
